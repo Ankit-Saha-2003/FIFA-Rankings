c8f439f56c9f0cbb74244bf9cbba97b8b187e4f8
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2022-06-26 13:11:04 +0530

    Displaying table on the webpage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import sqlite3
 
 # Configure application
@@ -12,6 +12,12 @@ conn = sqlite3.connect('rankings.db')
 conn.execute('CREATE TABLE IF NOT EXISTS rating (rank INT, team TEXT, rating FLOAT)')
 conn.close()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'hello'
+    if request.method == 'GET':
+        with sqlite3.connect('rankings.db') as conn:
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM rating')
+            rows = cur.fetchall()
+            conn.commit()
+        return render_template('index.html', rows=rows)
