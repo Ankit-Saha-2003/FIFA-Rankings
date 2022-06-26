faff9d8647e679861c6d606bca7086b2444d5e75
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2022-06-26 17:06:34 +0530

    Implemented web scraping to obtain top 20 rankings
    The database is automatically populated with the top 20 ranked teams obtained by web scraping the official Wikipedia page for FIFA World Rankings

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 # Configure application
 app = Flask(__name__)
@@ -13,6 +16,21 @@ conn.execute('CREATE TABLE IF NOT EXISTS rating (team TEXT, rating FLOAT)')
 cur = conn.cursor()
 cur.execute('DELETE FROM rating')
 conn.commit()
+
+# Loading the top 20 ranked teams into the database by web scraping the official Wikipedia page for FIFA Ranking
+URL = 'https://en.wikipedia.org/wiki/FIFA_Men%27s_World_Rankings'
+page = requests.get(URL)
+soup = BeautifulSoup(page.text, 'lxml')
+table1 = soup.find('table')
+for j in table1.find_all('tr')[3:23]:
+    row_data = j.find_all('td')
+    row = []
+    row.append(row_data[2].text.lstrip('\xa0'))
+    row.append(row_data[3].text.rstrip('\n'))
+    cur = conn.cursor()
+    cur.execute("INSERT INTO rating (team, rating) VALUES(?, ?)", (row[0], row[1]))
+    conn.commit()
+
 conn.close()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,6 +41,7 @@ def index():
         cur = conn.cursor()
         cur.execute('SELECT ROW_NUMBER() OVER(ORDER BY rating DESC) rank, team, rating FROM rating')
         rows = cur.fetchall()
+        conn.close()
         return render_template('index.html', rows=rows)
 
     else:
@@ -30,8 +49,8 @@ def index():
         rating = request.form.get("rating")
 
         if team and rating:
-            with sqlite3.connect("rankings.db") as con:
-                cur = con.cursor()
+            with sqlite3.connect("rankings.db") as conn:
+                cur = conn.cursor()
                 cur.execute("INSERT INTO rating (team, rating) VALUES(?, ?)", (team, rating))
-                con.commit()
+                conn.commit()
         return redirect("/")
