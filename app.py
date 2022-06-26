c8021d596d119935a97c91ea3fc5a5db770394aa
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2022-06-26 16:34:29 +0530

    Added functionality to interactively add a team to the database
    - index.html now has an input form to enter team and rating
    
    - The table is displayed by automatically sorting all the teams in the database by their rating

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 # Configure application
@@ -9,15 +9,29 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configuring database
 conn = sqlite3.connect('rankings.db')
-conn.execute('CREATE TABLE IF NOT EXISTS rating (rank INT, team TEXT, rating FLOAT)')
+conn.execute('CREATE TABLE IF NOT EXISTS rating (team TEXT, rating FLOAT)')
+cur = conn.cursor()
+cur.execute('DELETE FROM rating')
+conn.commit()
 conn.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        with sqlite3.connect('rankings.db') as conn:
-            cur = conn.cursor()
-            cur.execute('SELECT * FROM rating')
-            rows = cur.fetchall()
-            conn.commit()
+        conn = sqlite3.connect('rankings.db')
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute('SELECT ROW_NUMBER() OVER(ORDER BY rating DESC) rank, team, rating FROM rating')
+        rows = cur.fetchall()
         return render_template('index.html', rows=rows)
+
+    else:
+        team = request.form.get("team")
+        rating = request.form.get("rating")
+
+        if team and rating:
+            with sqlite3.connect("rankings.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO rating (team, rating) VALUES(?, ?)", (team, rating))
+                con.commit()
+        return redirect("/")
