34e8730965a77dd1cfe30d296c345e4ead56a7bf
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2022-06-26 12:06:47 +0530

    Initial set-up

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import sqlite3
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure that templates are auto-reloaded whenever a file changes
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configuring database
+conn = sqlite3.connect('rankings.db')
+conn.execute('CREATE TABLE rating (rank INT, team TEXT, rating FLOAT)')
+conn.close()
+
+@app.route('/')
+def index():
+    pass
