36817f88cfeabc99f2b1b01aef51238045334f4d
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2022-06-29 23:33:09 +0530

    Minor changes
    Used 'with' for sqlite3.connect

--- app.py
+++ app.py
@@ -11,40 +11,37 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Configuring database
-conn = sqlite3.connect('rankings.db')
-conn.execute('CREATE TABLE IF NOT EXISTS rating (team TEXT, rating FLOAT)')
-cur = conn.cursor()
-cur.execute('DELETE FROM rating')
-conn.commit()
-
-# Loading the top 20 ranked teams into the database by web scraping the official Wikipedia page for FIFA Ranking
-URL = 'https://en.wikipedia.org/wiki/FIFA_Men%27s_World_Rankings'
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, 'lxml')
-table1 = soup.find('table')
-teams_list = []
-for j in table1.find_all('tr')[3:23]:
-    row_data = j.find_all('td')
-    row = []
-    row.append(row_data[2].text.lstrip('\xa0'))
-    row.append(row_data[3].text.rstrip('\n'))
+with sqlite3.connect('rankings.db') as conn:
+    conn.execute('CREATE TABLE IF NOT EXISTS rating (team TEXT, rating FLOAT)')
     cur = conn.cursor()
-    cur.execute('INSERT INTO rating (team, rating) VALUES(?, ?)', (row[0], row[1]))
+    cur.execute('DELETE FROM rating')
     conn.commit()
-    teams_list.append(row[0].lower())
 
-conn.close()
+    # Loading the top 20 ranked teams into the database by web scraping the official Wikipedia page for FIFA Ranking
+    URL = 'https://en.wikipedia.org/wiki/FIFA_Men%27s_World_Rankings'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, 'lxml')
+    table1 = soup.find('table')
+    teams_list = []
+    for j in table1.find_all('tr')[3:23]:
+        row_data = j.find_all('td')
+        row = []
+        row.append(row_data[2].text.lstrip('\xa0'))
+        row.append(row_data[3].text.rstrip('\n'))
+        cur = conn.cursor()
+        cur.execute('INSERT INTO rating (team, rating) VALUES(?, ?)', (row[0], row[1]))
+        conn.commit()
+        teams_list.append(row[0].lower())
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         # Displays the ranking table on the home page
-        conn = sqlite3.connect('rankings.db')
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-        cur.execute('SELECT ROW_NUMBER() OVER(ORDER BY rating DESC) rank, team, rating FROM rating')
-        rows = cur.fetchall()
-        conn.close()
+        with sqlite3.connect('rankings.db') as conn:
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute('SELECT ROW_NUMBER() OVER(ORDER BY rating DESC) rank, team, rating FROM rating')
+            rows = cur.fetchall()
         return render_template('index.html', rows=rows)
 
     else:
